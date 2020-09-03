65af8c3d53ff46f431444d590e3daa4fd040bf44
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-09-04 00:34:53 +0530

    spannable string added

--- app/src/main/java/com/example/whatsappclone/OTPActivity.kt
+++ app/src/main/java/com/example/whatsappclone/OTPActivity.kt
@@ -2,6 +2,12 @@ package com.example.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import kotlinx.android.synthetic.main.activity_o_t_p.*
 
 const val PHONE_NUMBER = "phoneNumber"
@@ -20,5 +26,23 @@ class OTPActivity : AppCompatActivity() {
     private fun initViews() {
         phoneNumber = intent.getStringExtra(PHONE_NUMBER)
         verifyTv.text = getString(R.string.verfiy_number, phoneNumber)
+        setSpannableString()
+    }
+
+    private fun setSpannableString() {
+        val span = SpannableString("Waiting to automatically detect an SMS sent to\\n ${phoneNumber} Wrong Number?")
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+
+            }
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+                ds.color = ds.linkColor
+            }
+        }
+        span.setSpan(clickableSpan, span.length - 13, span.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        waitingTv.movementMethod = LinkMovementMethod.getInstance()
+        waitingTv.text = span
     }
 }
