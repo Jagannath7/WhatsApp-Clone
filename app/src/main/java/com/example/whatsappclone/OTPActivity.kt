23733b39617b030c2aba819a3b694631f9547689
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-09-04 15:33:36 +0530

    timer for otp added

--- app/src/main/java/com/example/whatsappclone/OTPActivity.kt
+++ app/src/main/java/com/example/whatsappclone/OTPActivity.kt
@@ -1,7 +1,9 @@
 package com.example.whatsappclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.TextPaint
@@ -9,6 +11,7 @@ import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.View
 import kotlinx.android.synthetic.main.activity_o_t_p.*
+
 
 const val PHONE_NUMBER = "phoneNumber"
 
@@ -21,6 +24,21 @@ class OTPActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_o_t_p)
         initViews()
+        showCountDownTimer(60000)
+    }
+
+    private fun showCountDownTimer(milliSecInFuture: Long) {
+        resendBtn.isEnabled = false
+        object : CountDownTimer(milliSecInFuture, 1000){
+
+            override fun onTick(miliSecUntilFinish: Long) {
+                counterTv.text = getString(R.string.seconds_remaining, miliSecUntilFinish/1000)
+            }
+
+            override fun onFinish() {
+                resendBtn.isEnabled = true
+            }
+        }.start()
     }
 
     private fun initViews() {
@@ -33,7 +51,7 @@ class OTPActivity : AppCompatActivity() {
         val span = SpannableString("Waiting to automatically detect an SMS sent to\\n ${phoneNumber} Wrong Number?")
         val clickableSpan = object : ClickableSpan() {
             override fun onClick(widget: View) {
-
+                startLoginActivity()
             }
             override fun updateDrawState(ds: TextPaint) {
                 super.updateDrawState(ds)
@@ -45,4 +63,11 @@ class OTPActivity : AppCompatActivity() {
         waitingTv.movementMethod = LinkMovementMethod.getInstance()
         waitingTv.text = span
     }
+
+    private fun startLoginActivity() {
+        startActivity(Intent(this, LoginActivity::class.java)
+            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+    }
+
+    override fun onBackPressed() {}
 }
