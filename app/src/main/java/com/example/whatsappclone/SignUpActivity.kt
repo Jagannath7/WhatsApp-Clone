a51aac0fcb968903dee1c040be7b9b7bd860f3b9
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-09-06 00:18:50 +0530

    select image from gallery

--- app/src/main/java/com/example/whatsappclone/SignUpActivity.kt
+++ app/src/main/java/com/example/whatsappclone/SignUpActivity.kt
@@ -1,11 +1,65 @@
 package com.example.whatsappclone
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        userImgView.setOnClickListener {
+            checkForImagePermission()
+        }
+    }
+
+    private fun checkForImagePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+           if((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) &&
+               (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+           ){
+                val permissionRead = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                val permisssionWrite = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+                requestPermissions(
+                    permissionRead,
+                    1001
+                )
+
+                requestPermissions(
+                    permisssionWrite,
+                    1002
+                )
+
+           }else{
+               pickImageFromGallery()
+           }
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(
+            intent,
+            1000
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == 1000){
+            data?.data?.let {
+                userImgView.setImageURI(it)
+            }
+        }
     }
 }
