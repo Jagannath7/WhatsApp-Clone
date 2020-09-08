1299b91ae06634fae67931a17cade11b93fa5af2
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-09-08 23:07:13 +0530

    implemented pagelist for users

--- app/src/main/java/com/example/whatsappclone/fragments/PeopleFragment.kt
+++ app/src/main/java/com/example/whatsappclone/fragments/PeopleFragment.kt
@@ -5,23 +5,80 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.whatsappclone.R
 import com.example.whatsappclone.UserViewHolder
 import com.example.whatsappclone.models.User
 import com.firebase.ui.firestore.paging.FirestorePagingAdapter
+import com.firebase.ui.firestore.paging.FirestorePagingOptions
+import com.firebase.ui.firestore.paging.LoadingState
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.fragment_chats.*
+import java.lang.Exception
 
 class PeopleFragment : Fragment() {
 
     lateinit var madapter: FirestorePagingAdapter<User, UserViewHolder>
+
+    val auth by lazy{
+        FirebaseAuth.getInstance()
+    }
+
+    val database by lazy{
+        FirebaseFirestore.getInstance().collection("users")
+            .orderBy("name", Query.Direction.DESCENDING)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setUpAdapter()
         return inflater.inflate(R.layout.fragment_chats,container,false)
+    }
+
+    private fun setUpAdapter() {
+        val config = PagedList.Config.Builder()
+            .setPrefetchDistance(2)
+            .setPageSize(10)
+            .setEnablePlaceholders(false)
+            .build()
+
+        val options = FirestorePagingOptions.Builder<User>()
+            .setLifecycleOwner(viewLifecycleOwner)
+            .setQuery(database, config, User::class.java)
+            .build()
+
+        madapter = object: FirestorePagingAdapter<User, UserViewHolder>(options){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+                val view = layoutInflater.inflate(R.layout.list_item, parent, false)
+                return  UserViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: User) {
+                holder.bind(user = model)
+            }
+
+            override fun onLoadingStateChanged(state: LoadingState) {
+                super.onLoadingStateChanged(state)
+                when(state){
+                    LoadingState.LOADING_INITIAL -> {}
+                    LoadingState.LOADING_MORE -> {}
+                    LoadingState.LOADED -> {}
+                    LoadingState.FINISHED -> {}
+                    LoadingState.ERROR -> {}
+                }
+            }
+
+            override fun onError(e: Exception) {
+                super.onError(e)
+            }
+
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
